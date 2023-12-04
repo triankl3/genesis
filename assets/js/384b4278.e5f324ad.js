64ba03b5cf36f936da7a3e8318f6b86c8cf0c428
triankl3 <25014845+triankl3@users.noreply.github.com>
2023-12-04 11:10:20 +0100

    Built and published by Moonwave

---
+++ assets/js/384b4278.e5f324ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[417],{3891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/triankl3/genesis/blob/main","baseUrl":"/genesis/","classOrder":[],"apiCategories":[]}')}}]);
